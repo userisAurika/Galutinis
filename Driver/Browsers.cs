1ea37fcd571f2d8107fa17c186a1d44e44b200bf
Aurika -iene <aurikaa@gmail.com>
2021-11-21 13:53:47 +0200

    Driver

---
+++ Driver/Browsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatinisTestavimas2.Driver
+{
+   enum Browsers//nekintanti statiniai dalykai
+    {
+        Chrome,
+        IncognitoChrome,
+        Firefox,
+        Opera,
+        Edge,
+        Safari
+    }
+}
---
+++ Driver/CustomeDriver.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatinisTestavimas2.Driver
+{
+    public class CustomeDriver
+    {
+        public static IWebDriver GetChromeDriver() 
+        {
+            return GetDriver(Browsers.Chrome);
+        }
+        public static IWebDriver GetFirefoxDriver()
+        {
+            return GetDriver(Browsers.Firefox);
+        }
+        public static IWebDriver GetIncognitoChrome()
+        {
+            return GetDriver(Browsers.IncognitoChrome);
+        }
+
+        private static IWebDriver GetDriver(Browsers browserName) 
+        {
+            IWebDriver driver = null;
+            switch (browserName)
+            {
+                case Browsers.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+                case Browsers.Firefox:
+                    driver = new FirefoxDriver();
+                    break;
+                case Browsers.IncognitoChrome:
+                    driver = GetChromeWithOptions();
+                    break;
+
+            }
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+
+            return driver;
+        }
+        private static IWebDriver GetChromeWithOptions() 
+           {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("incognito");
+            option.AddArgument("start-maximized");
+            //options.AddArguments("start-maximized,incognito")
+            return new ChromeDriver(option);
+        }
+    }
+}
